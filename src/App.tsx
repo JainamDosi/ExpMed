e710a31fdd709ab3b6c069d65c8aa506d7662dfc
Jainam <127018175+JainamDosi@users.noreply.github.com>
2026-02-07 21:01:14 +0530

    analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import { Analytics } from '@vercel/analytics/react';
 import LandingPage from './components/LandingPage';
 import MainService from './components/MainService';
 
 const App: React.FC = () => {
   return (
-    <Routes>
-      <Route path="/" element={<LandingPage />} />
-      <Route path="/analyze" element={<MainService />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/analyze" element={<MainService />} />
+      </Routes>
+      <Analytics />
+    </>
   );
 };
 
